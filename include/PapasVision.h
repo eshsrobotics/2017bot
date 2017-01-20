6dc24872ca38e49ccf56d917bcf4823def64909e
Ari <applemaster.ari@gmail.com>
2017-01-19 20:21:22 -0800

    Added a header and implemented it into the main.cpp
    I added a header that is called PapasVision that lets us run the C++ code from an ubuntu machien smoothly. The code right now is just printing out lines saying that it runs.

---
+++ include/PapasVision.h
@@ -0,0 +1,11 @@
+#ifndef PAPAS_VISION_H__
+#define PAPAS_VISION_H__
+
+class PapasVision {
+   public:
+       PapasVision();
+   private:
+       // Private stuff goes here
+};
+
+#endif // (#ifndef PAPAS_VISION_H__)
