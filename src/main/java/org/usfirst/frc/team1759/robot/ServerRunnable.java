04582ca7aa0f0f0bd3b1598dde4187acae32ba85
Ari Berkowicz <applemaster.ari@gmail.com>
2017-01-28 21:19:17 -0800

    Figured out how to do the server-side thread in Java.  Updated ServerRunnable.
    * ServerRunnable.java: The key to getting everything working was making
      sure that a disconnection caused the recreation of the socket with a new
      accept() call.  We are now robust to client-side network failure.

--- src/main/java/org/usfirst/frc/team1759/robot/ServerRunnable.java
+++ src/main/java/org/usfirst/frc/team1759/robot/ServerRunnable.java
@@ -3,9 +3,12 @@ package org.usfirst.frc.team1759.robot;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.PrintWriter;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import javax.xml.parsers.DocumentBuilder;
@@ -13,59 +16,75 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 /**
-* @author Ari Berkowicz and Ryan Lim
-*
-* This is just a Runnable to host our network-listening code.  Better that we
-* idle than the
-* main driver thread.
-*/
+ * @author Ari Berkowicz and Ryan Lim
+ *
+ *         This is just a Runnable to host our network-listening code. Better
+ *         that we idle than the main driver thread.
+ */
 public class ServerRunnable implements Runnable {
-  /**
-   * Any target goal that is greater than this distance will be rejected by
-   * findgoal().
-   */
+	/**
+	 * Any target goal that is greater than this distance will be rejected by
+	 * findgoal().
+	 */
 
-  // When set to true the camerathread will kill itself.
-  public static boolean killCameraThread = false;
+	// When set to true the camerathread will kill itself.
+	public static final int PORT = 12345;
+	public static boolean killCameraThread = false;	
 
-  public static int sleepTimeMillisecond = 1;
+	// This variable gives us one place to tweak the amount of time the camera
+	// thread
+	// will sleep before taking up CPU again. It also helps for the end of the
+	// program:
+	// after the main thread sets our death flag above, it only needs to wait
+	// for about
+	// this many milliseconds for *us* to die before it does itself.
 
-  // This variable gives us one place to tweak the amount of time the camera
-  // thread
-  // will sleep before taking up CPU again. It also helps for the end of the
-  // program:
-  // after the main thread sets our death flag above, it only needs to wait
-  // for about
-  // this many milliseconds for *us* to die before it does itself.
+	@Override
+	public void run() {
+		Socket clientSocket = null;
+		
+		try (ServerSocket socket = new ServerSocket(PORT)) {
 
-  @Override
-  public void run() {
-    // TODO Auto-generated method stub
+			System.out.println("Waiting for client connection.");
+			clientSocket = socket.accept();
+			
+			System.out.println("Entering waiting loop.");
+			while (killCameraThread == false) {
+				try {
+									
+					InputStream inputStream = clientSocket.getInputStream();
+					BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+					String s = reader.readLine();
+	
+					String address = "";
+					byte[] rawAddress = clientSocket.getInetAddress().getAddress();
+					if (rawAddress.length == 4) {
+						address = clientSocket.getInetAddress().getHostName(); // Likely
+																				// IPv4.
+					} else {
+						address = "[" + clientSocket.getInetAddress().getHostName() + "]"; // Likely
+																							// IPv6.
+					}
+					System.out.println("Got \"" + s + "\" from " + address + ":" + clientSocket.getPort());
+				
+				} catch (IOException e) {
+					
+					// Perhaps the socket disconnected.  Perhaps the network went down.   Whatever
+					// happened, it's not worth worrying about.  Just wait for a reconnection from
+					// the client.
+					System.err.println("Caught an IO exception: " + e.getMessage() + ".  Waiting for new client connection.");
+					clientSocket.close();
+					clientSocket = socket.accept();
+				} 
+			}
+					
+		} catch (Throwable e) {
+			// Gets rid of all the errors from before.
+			System.err.println("Caught a fatal exception: " + e.getMessage());
+		}
+	}
 
-    while (killCameraThread == false) {
-      try {
-        int i = 1;
-        Socket socket = null;
-        InetAddress host = null;
-        host.getAllByName("10.17.59.2");
-
-        socket = new Socket(host, 22);
-
-        File file = new File("C:\\test.xml");
-        // Get the size of the file
-        long length = file.length();
-        byte[] bytes = new byte[16 * 1024];
-        InputStream in = new FileInputStream(file);
-        OutputStream out = socket.getOutputStream();
-        out.close();
-        in.close();
-        socket.close();
-      } catch (Throwable e) {
-        // Gets rid of all the errors from before.
-        System.err.println();
-      }
-    }
-  }
-
-  public void die() { killCameraThread = true; }
+	public void die() {
+		killCameraThread = true;
+	}
 }
