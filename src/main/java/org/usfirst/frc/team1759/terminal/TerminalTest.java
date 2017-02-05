c3e70e940805d36f78fa7de2cf56b8e7f4e2beb2
applemaster2000 <applemaster.ari@gmail.com>
2017-02-04 20:44:02 -0800

    Got command-line JAR building to work, though it's inelegant and gauche.
    To build TerminalTest.jar, run "ant create_command_line_jar".
    
    To run TerminalTest.jar, do "java -jar bin/TerminalTest.jar".
    
    Now to put the sax parser into this thing.

---
+++ src/main/java/org/usfirst/frc/team1759/terminal/TerminalTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.usfirst.frc.team1759.terminal;
+
+/**
+ * @author frcprogramming
+ *
+ */
+public class TerminalTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, Java!");
+	}
+
+}
