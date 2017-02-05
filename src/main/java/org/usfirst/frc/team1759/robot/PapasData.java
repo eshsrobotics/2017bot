0dc02d94474dd3451e2641ca7d2333aaa523208c
applemaster2000 <applemaster.ari@gmail.com>
2017-02-04 20:06:20 -0800

    Committing what I think is a completed SAX parser...but.
    I need a command-line dirver to test it.  That will be adapted from
    some code that I just pushed.

---
+++ src/main/java/org/usfirst/frc/team1759/robot/PapasData.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team1759.robot;
+
+/**
+ * A struct that holds the data we get back from PapasVision in an easy-to-digest format. 
+ * 
+ * It's not a big deal.  It's not managed.  It's just a tuple.
+ */
+public class PapasData {	
+	public double papasDistanceInInches = -1;
+	public double papasAngleInDegrees = -1;
+	public boolean solutionFound = false;
+	public String solutionType = "";
+}
