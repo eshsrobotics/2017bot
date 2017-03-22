6107f79c8ad3e8ba2d64df5aee8eabb8e4318434
uakotaobi <uche.robotics@akotaobi.org>
2017-03-21 17:59:15 -0700

    Integrated the shooter command into Robot.java, added to OI and RobotMap as needed. Ready to test the shooter. . .finally.

--- src/main/java/org/usfirst/frc/team1759/robot/OI.java
+++ src/main/java/org/usfirst/frc/team1759/robot/OI.java
@@ -42,7 +42,7 @@ public class OI {
 	// until it is finished as determined by it's isFinished method.
 	// button.whenReleased(new ExampleCommand());
 
-	private Joystick leftStick;
+	Joystick leftStick;
 	private Joystick rightStick;
 	private Joystick shootStick; 
 
@@ -62,8 +62,15 @@ public class OI {
 	 * on the leftStick for the time being. 
 	 */
 	public static final int AUTO_AIM_BUTTON_NUMBER = 2;
+	public static final int GYRO_SWITCH_BUTTON_NUMBER = 11;
+	public static final int INCREASE_SPEED = 12;
+	public static final int DECREASE_SPEED = 11;
 
-	private Button autoAimButton = new JoystickButton(leftStick, AUTO_AIM_BUTTON_NUMBER);
+	Button autoAimButton = new JoystickButton(leftStick, AUTO_AIM_BUTTON_NUMBER);
+	Button driveSwitch = new JoystickButton(rightStick, GYRO_SWITCH_BUTTON_NUMBER);
+	Button goFast = new JoystickButton(leftStick, INCREASE_SPEED);
+	Button goSlow = new JoystickButton(rightStick, DECREASE_SPEED);
+	
 	
 	/**
 	 * Constructs this input interface and provides it with everything it needs to control
