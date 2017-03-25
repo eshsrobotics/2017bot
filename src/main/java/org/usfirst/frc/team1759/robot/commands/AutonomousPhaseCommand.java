f73bb2db448f67b1f94683ee890cb0752725f90b
Ari Berkowicz <applemaster.ari@gmail.com>
2017-03-25 14:37:09 -0700

    There we go. Autonomous code is written; this will be tagged. Our autonomous code will drive forward and take us to the line, though the time limit may need a little bit of adjusting.

--- src/main/java/org/usfirst/frc/team1759/robot/commands/AutonomousPhaseCommand.java
+++ src/main/java/org/usfirst/frc/team1759/robot/commands/AutonomousPhaseCommand.java
@@ -1,5 +1,34 @@
 package org.usfirst.frc.team1759.robot.commands;
 
-public class AutonomousPhaseCommand {
+import org.usfirst.frc.team1759.robot.subsystems.MecanumDriveSubSystem;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class AutonomousPhaseCommand extends Command {
+	private Joystick joystick;
+	private MecanumDriveSubSystem papasDrive;
+	
+	public AutonomousPhaseCommand(Joystick joystick, MecanumDriveSubSystem papasDrive) {
+		this.joystick = joystick;
+		this.papasDrive = papasDrive;
+		requires(papasDrive);
+	}
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	public void execute() {
+		papasDrive.autonomousDrive(0, .75, 0);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		papasDrive.autonomousDrive(0, 0, 0);
+	}
 
 }
